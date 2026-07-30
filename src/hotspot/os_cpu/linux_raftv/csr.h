79e950ef7ee442f4f7aa93c9a3d0952d5ce7c50e
Karl Hallsby <karl@hallsby.com>
2026-07-30 15:46:07 -0500

    Add rest of KBE CSRs

--- src/hotspot/os_cpu/linux_raftv/csr.h
+++ src/hotspot/os_cpu/linux_raftv/csr.h
@@ -4,6 +4,9 @@
 #define CSR_USCRATCH 0x840
 #define CSR_UEPC     0x841
 #define CSR_UCAUSE   0x842
+#define CSR_UTVAL    0x843
+#define CSR_USSCRATCH 0x844
+#define CSR_UALREADY_HANDLING 0x845
 
 /* FP Trap CSR */
 #define CSR_FFLAGS_CARE 0x880
